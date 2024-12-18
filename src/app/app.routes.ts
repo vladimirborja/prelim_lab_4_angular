f781545cb2e81f4c3f1566507c06ed73ef721544
System Administrator <root@Vladimirs-Mac-mini.local>
2024-12-18 23:01:30 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './admin/home/home.component';
+import { UserComponent } from './admin/user/user.component';
+import { PostComponent } from './admin/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'user', component : UserComponent},
+    {path : 'post', component : PostComponent},
+    {path : '', component : HomeComponent}
+];
